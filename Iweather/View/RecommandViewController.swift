2bc966f6d4ee81540be034132dd5ad55ea684d49
“hw3590” <lkhs44@naver.com>
2023-10-02 18:49:46 +0900

    [FEAT] 일몰 일출 낮 밤 시간별 이미지 표현

--- Iweather/View/RecommandViewController.swift
+++ Iweather/View/RecommandViewController.swift
@@ -4,17 +4,18 @@
 //
 //  Created by Hyunwoo Lee on 2023/09/27.
 //
+//
 
 import UIKit
 import SnapKit
+import Moya
 
 class RecomanndViewController: UIViewController {
-    
-    private var recommandViewModel: RecommandViewModel!
-    
+ 
+    private let weatherService = MoyaProvider<WeatherAPI>(plugins: [NetworkLoggerPlugin()])
+     
     private var backImage: UIImageView = {
         let backImage = UIImageView()
-//        backImage.image = UIImage(named: bac)
         return backImage
     }()
 
@@ -37,7 +38,6 @@ class RecomanndViewController: UIViewController {
     
     private var commentLabel: UILabel = {
         let commentLabel = UILabel()
-  //      commentLabel.font = UIFont.boldSystemFont(ofSize: 30)
         commentLabel.text = "스파가기 좋은날"
         commentLabel.textColor = .white
         commentLabel.font = UIFont(name: "GmarketSansBold", size: 30)
@@ -84,9 +84,13 @@ class RecomanndViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+   
         initConstraints()
         buttonSetup()
+        
+        let exclude = "hourly,daily"
+        getWeatherData(exclude: exclude)
     }
 }
 
@@ -144,7 +148,69 @@ extension RecomanndViewController {
     @objc func backBtnTapped() {
         self.dismiss(animated: true, completion: nil)
     }
+    private func getWeatherData(exclude: String) {
+           print("요청중")
+           weatherService.request(.getWeatherForCity("busan", days: 5)) { result in
+               switch result {
+               case let .success(response):
+                   do {
+                       let welcome = try response.map(Welcome.self)
+                       DispatchQueue.main.async {
+                           self.updateUI(with: welcome)
+                       }
+                   } catch {
+                       self.showError(message: "날씨 정보를 파싱하는 중 오류가 발생했습니다: \(error)")
+                   }
+               case let .failure(error):
+                   self.showError(message: "날씨 정보를 가져오는 중 오류가 발생했습니다: \(error.localizedDescription)")
+                   print("네트워크 에러: \(error)")
+               }
+           }
+       }
+    private func updateUI(with welcome: Welcome) {
+          print("UI 업데이트")
+        // 현재 시간을 가져옵니다.
+          let currentTime = Date()
+          // 날출데이터에서 일출 및 일몰 시간을 계산합니다.
+          let (sunriseStart, sunriseEnd, sunsetStart, sunsetEnd) = calculateSunriseAndSunsetTimes(weatherResponse: welcome)
+          // 현재 시간을 기반으로 어떤 이미지를 보여줄지 결정합니다.
+          determinePhotoToShow(currentTime: currentTime, sunriseStart: sunriseStart, sunriseEnd: sunriseEnd, sunsetStart: sunsetStart, sunsetEnd: sunsetEnd)
+        print(sunsetEnd)
+        print(sunsetStart)
+        print(sunriseEnd)
+        print(sunriseStart)
+      }
+    func calculateSunriseAndSunsetTimes(weatherResponse: Welcome) -> (Date, Date, Date, Date) {
+          let sunrise = Date(timeIntervalSince1970: TimeInterval(weatherResponse.city.sunrise))
+          let sunset = Date(timeIntervalSince1970: TimeInterval(weatherResponse.city.sunset))
+          let sunriseStart = sunrise.addingTimeInterval(-30 * 60)
+          let sunriseEnd = sunrise.addingTimeInterval(30 * 60)
+          let sunsetStart = sunset.addingTimeInterval(-30 * 60)
+          let sunsetEnd = sunset.addingTimeInterval(30 * 60)
+          return (sunriseStart, sunriseEnd, sunsetStart, sunsetEnd)
+      }
     
-   }
-
-
+    func determinePhotoToShow(currentTime: Date, sunriseStart: Date, sunriseEnd: Date, sunsetStart: Date, sunsetEnd: Date) {
+        // 일출 진행중 사진
+        if currentTime >= sunriseStart && currentTime <= sunriseEnd {
+            return backImage.image = UIImage(named: "1")
+        }
+        // 일몰 진행중 사진
+        if currentTime >= sunsetStart && currentTime <= sunsetEnd {
+            return backImage.image = UIImage(named: "2")
+        }
+        // 밤 사진
+        if currentTime >= sunsetEnd || currentTime <= sunriseStart {
+            return backImage.image = UIImage(named: "3")
+        }
+        // 낮 사진
+        return backImage.image = UIImage(named: "4")
+    }
+    
+    private func showError(message: String) {
+        let alertController = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
