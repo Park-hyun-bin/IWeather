3d85ed463520ad282ad7090aa9e3e837c697dff7
Park-Hyun-Bin <gusqls9861@gmail.com>
2023-10-06 14:58:00 +0900

    [Feat]: MapView에 TableViewCell에 애니메이션 추가 중간과정

--- Iweather/View/UIViewItem/CustomTableViewCell.swift
+++ Iweather/View/UIViewItem/CustomTableViewCell.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import Gifu
 
 class CustomTableViewCell: UITableViewCell {
     var iconImageView: UIImageView!
     var label: UILabel!
+    var backgroundImageView: GIFImageView!
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundImageView = GIFImageView(frame: contentView.bounds)
+        backgroundImageView.contentMode = .scaleAspectFill
+        contentView.addSubview(backgroundImageView)
+
+        contentView.sendSubviewToBack(backgroundImageView)
 
         iconImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         iconImageView.contentMode = .scaleAspectFit
@@ -21,11 +29,11 @@ class CustomTableViewCell: UITableViewCell {
         label = UILabel(frame: CGRect(x: 40, y: 0, width: contentView.frame.width - 40, height: 30))
         label.textColor = .black
         contentView.addSubview(label)
-        
+
         self.layer.cornerRadius = 10.0
         self.layer.masksToBounds = true
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
